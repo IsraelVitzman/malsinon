c14de42ff23dd2a40f82e192e59a54b990c22e0f
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-09 17:47:20 +0300

    vers

---
+++ ConsoleApp34/ToRecruit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp34
+{
+    internal class ToRecruit
+    {   
+        Database db =new Database();
+        public void recruit()
+        {
+            string qeerry = "FROM * SELECT  Reports WHERE ReporterId > 10 ";
+
+            MySqlConnection connection = db.connection();
+
+            MySqlCommand command = new MySqlCommand(qeerry,connection);
+
+            MySqlDataReader reader = command.ExecuteReader();
+            while (reader.Read()) 
+            { 
+                Console.WriteLine(reader);
+            }
+
+        }
+    }
+}
