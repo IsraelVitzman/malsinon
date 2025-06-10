1bb805c88c6afb00e77dc83c18387127a5d5532d
Israel Vitzman <iv058.l.v@gmail.com>
2025-06-10 12:36:03 +0300

    g

--- ConsoleApp34/alarts.cs
+++ ConsoleApp34/alarts.cs
@@ -1,12 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ConsoleApp34;
+using MySql.Data.MySqlClient;
 
-namespace ConsoleApp34
+public class AlertHandler
 {
-    internal class alarts
+    Database db = new Database();
+    public MySqlDataReader newAlert()
     {
+        string qeerry = "SELECT r1.TargetId, r1.SubmissionTime" +
+                    "    FROM Reports r1" +
+                    "    JOIN Reports r2 ON r1.TargetId = r2.TargetId" +
+                    "    AND r2.SubmissionTime BETWEEN r1.SubmissionTime AND DATE_ADD(r1.SubmissionTime, INTERVAL 15 MINUTE)" +
+                    "    GROUP BY r1.TargetId, r1.SubmissionTime" +
+                    "    HAVING COUNT(r2.Id) >= 3;";
+
+        MySqlConnection connection = db.connection();
+        connection.Open();
+
+        MySqlCommand command = new MySqlCommand(qeerry, connection);
+
+        MySqlDataReader reader = command.ExecuteReader();
+
+        return reader;
+
     }
+    public  void InsertAlert(int targetId, string alertType, string reason)
+    {      
+            
+
+            MySqlConnection conn = db.connection();
+        
+            conn.Open();
+
+
+            string sql = "INSERT INTO Alerts (TargetId, AlertType) VALUES (@TargetId, @AlertType)";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+            cmd.Parameters.AddWithValue("@TargetId", targetId);
+            cmd.Parameters.AddWithValue("@AlertType", alertType);
+            
+
+            cmd.ExecuteNonQuery();
+        
+    }
+    public void RunAlertAnalysis()
+    {
+        using (MySqlDataReader reader = newAlert())
+        {
+            while (reader.Read())
+            {
+                int targetId = reader.GetInt32("TargetId");
+                DateTime time = reader.GetDateTime("SubmissionTime");
+
+                string alertType = "Burst";
+                string reason = $"3 דיווחים על מטרה תוך פחות מ־15 דקות. זמן התחלה: {time}";
+
+                
+                InsertAlert(targetId, alertType, reason);
+
+                Console.WriteLine($"התראה נוספה: מטרה {targetId} נחשבת למסוכנת (Burst)");
+            }
+
+            reader.Close();
+        }
+    }
+
 }
+
--- ConsoleApp34/creetTeble.cs
+++ ConsoleApp34/creetTeble.cs
@@ -9,16 +9,16 @@ using System;
 
       public void CreateNewTable()
       {
-        string newTable = @"
+        string newTable1 = @"
                CREATE TABLE IF NOT EXISTS People (
                         Id INT PRIMARY KEY AUTO_INCREMENT,
                         Name VARCHAR(100) NOT NULL,
                         SecretCode VARCHAR(50) NOT NULL,
                        
                         CreatedDate DATETIME DEFAULT CURRENT_TIMESTAMP
-               );
-               
-               CREATE TABLE IF NOT EXISTS Reports (
+               )";
+
+        string newTable2 = @"      CREATE TABLE IF NOT EXISTS Reports (
                      Id INT PRIMARY KEY AUTO_INCREMENT,
                      ReporterId INT NOT NULL,
                      TargetId INT NOT NULL,
@@ -27,23 +27,30 @@ using System;
 
                      FOREIGN KEY (ReporterId) REFERENCES People(Id),
                      FOREIGN KEY (TargetId) REFERENCES People(Id)
-               );
-                CREATE TABLE Alerts (
+               )";
+
+        string newTable3 = @"       CREATE TABLE Alerts (
                      AlertId INT AUTO_INCREMENT PRIMARY KEY,
                      TargetId INT NOT NULL,
                      AlertType ENUM('THRESHOLD', 'BURST') NOT NULL,
                    
     
-                     FOREIGN KEY (TargetId) REFERENCES People(PersonId)
+                     FOREIGN KEY (TargetId) REFERENCES People(Id)
                 );
-        ";
+                ";
 
 
         try
         {
             MySqlConnection con = db.connection();
-            MySqlCommand cmd = new MySqlCommand(newTable, con);
-            cmd.ExecuteNonQuery();
+            MySqlCommand cmd1 = new MySqlCommand(newTable1, con);
+            MySqlCommand cmd2 = new MySqlCommand(newTable2, con);
+            MySqlCommand cmd3 = new MySqlCommand(newTable3, con);
+
+            cmd1.ExecuteNonQuery();
+            cmd2.ExecuteNonQuery();
+            cmd3.ExecuteNonQuery();
+
             Console.WriteLine("Tables created successfully...");
             db.close(con);
         }
